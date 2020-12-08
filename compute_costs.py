8334ed2dbc6cdac63e0a1c7dd565539bdd51bd66
manju <manju.garimella@lilly.com>
2020-12-08 16:08:07 -0500

    function that compute cost

--- compute_costs.py
+++ compute_costs.py
@@ -3,6 +3,8 @@ import os
 from aws_cost_calculator import AWSCalculator
 import csv
 
+data_path = "data/delta_10_07.csv"
+
 def read_csv(path):
     """[summary]
     read csv file
@@ -13,20 +15,122 @@ def read_csv(path):
         lines = f.read().splitlines()    
         return lines
 
-def process_batch_resource_cnt():
+def process_batch_cost(records):
+    aws_calc = AWSCalculator()
+
+    ## 1. load_parameters lambda
     ## lambda
-    
+    load_parameters_memory = 2240
+    ## msec
+    load_parameters_time = 900 * 1000
+
+    ## 2. statemachine
     ## happy path :) only
     statemachine_transitions = 5
     num_parallel = 2  ## 50 cnt
     
+    ## 3. process batch lambda
+    process_batch_memory = 3008
+    process_batch_time = 900 * 1000
 
-def transform_resource_cnt():
-    pass
+    ## 4. job complete lambda
+    job_complete_memory = 128
+    job_complete_time = 900 * 1000
+    
+    ## data
+    num_records = len(records)
 
-def enrich_resource_cnt():
-    pass
+    load_parameters_lambda_execution = 1
+    statemachine_invocations = num_records // 100
+    process_batch_lambda_execution = statemachine_invocations * 2 ## 2 times each statemachine invocation
+    job_complete_lambda_execution = statemachine_invocations
 
+    ## cost 
+    cost = aws_calc.lambda_pricing(load_parameters_lambda_execution, load_parameters_memory, load_parameters_time) + aws_calc.statemachine_costs(statemachine_invocations, num_parallel, statemachine_transitions) + \
+        aws_calc.lambda_pricing(process_batch_lambda_execution, process_batch_memory, process_batch_lambda_execution) + \
+        aws_calc.lambda_pricing(
+            job_complete_lambda_execution, job_complete_memory, job_complete_time)
+    
+    return cost
+
+def transform_cost(records):
+    aws_calc = AWSCalculator()
+
+    # 1. extractPDF lambda
+    extract_pdf_lambda_memory = 2240
+    extract_pdf_lambda_timeout = 900 * 1000
+
+    # 2. textract
+    
+    # 3. pdf to text lambda
+    extract_pdftotxt_lambda_memory = 2240
+    extract_pdftotxt_lambda_timeout = 900 * 1000
+
+    csv_reader = csv.DictReader((line for line in records if not line.isspace(
+    ) and not line.replace(",", "").isspace()), delimiter=',')
+
+    csv_reader = list(csv_reader)
+    num_pages = list(map(lambda x: int(x['num_pages']), csv_reader))
+    num_records = len(csv_reader)
+    
+    cost = aws_calc.lambda_pricing(num_records, extract_pdf_lambda_memory, extract_pdf_lambda_timeout) + aws_calc.textract_costs(sum(num_pages)) + aws_calc.lambda_pricing(num_records, extract_pdftotxt_lambda_memory, extract_pdftotxt_lambda_timeout)
+
+    return cost
+
+def enrich_cost(records):
+    aws_cost = AWSCalculator()
+    num_records = len(records)
+
+    # enrich invoke statemachie lambda
+    enrich_invoke_statemachine_lambda_memory = 2240
+    enrich_invoke_statemachine_lambda_time = 900 * 1000
+
+    # enrich statemachine
+    number_of_transitions = 7
+    number_of_parallel = 1
+
+    # negation lambda
+    enrich_nd_lambda_memory = 2240
+    enrich_nd_lambda_time = 900 * 1000
+
+    # sd lambda
+    enrich_sd_lambda_memory = 2240
+    enrich_sd_lambda_time = 900 * 1000
+    
+    # pfRA lambda
+    enrich_pfRa_lambda_memory = 2240
+    enrich_pfRa_lambda_time = 900 * 1000
+    
+    # jc lambda
+    enrich_jc_lambda_memory = 2240
+    enrich_jc_lambda_time = 900 * 1000
+
+    cost = aws_cost.lambda_pricing(num_records, enrich_invoke_statemachine_lambda_memory, enrich_invoke_statemachine_lambda_time) + aws_cost.statemachine_costs(num_records, number_of_parallel, number_of_transitions) + aws_cost.lambda_pricing(
+        num_records, enrich_nd_lambda_memory, enrich_nd_lambda_time) + aws_cost.lambda_pricing(num_records, enrich_sd_lambda_memory, enrich_sd_lambda_time) + aws_cost.lambda_pricing(num_records, enrich_pfRa_lambda_memory, enrich_pfRa_lambda_time) + aws_cost.lambda_pricing(num_records, enrich_jc_lambda_memory, enrich_jc_lambda_time)
+
+    return cost
+    
+
+    
+
+
+if __name__ == "__main__":
+    # read data
+    ## read csv file - divide into chunks of 100 - invoke statemachine (each invocation 2 parallel paths)
+    records = read_csv(data_path)
+
+    # process batch costs
+    pb_costs = process_batch_cost(records)
+    
+    ## transform costs
+    trans_costs = transform_cost(records)
+
+    # enrich costs
+    en_costs = enrich_cost(records)
+
+    print(pb_costs, trans_costs, en_costs)
+    print('Total cost', sum([pb_costs, trans_costs, en_costs]))
+    
     
 
 
